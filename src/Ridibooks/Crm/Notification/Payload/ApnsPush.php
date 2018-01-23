140566f0dbbaf95d46973e0089b4f82e088ec5da
서지혁 <limeburst@ridi.com>
2018-01-23 17:22:33 +0900

    APNS 무음 푸시

--- src/Ridibooks/Crm/Notification/Payload/ApnsPush.php
+++ src/Ridibooks/Crm/Notification/Payload/ApnsPush.php
@@ -11,6 +11,7 @@ class ApnsPush implements \JsonSerializable
     private $message;
     private $url;
     private $identifier;
+    private $force_silent;
 
     /**
      * APNS 페이로드 생성자.
@@ -19,13 +20,20 @@ class ApnsPush implements \JsonSerializable
      * @param string $message 푸시 알림 본문
      * @param string $url 알림을 눌렀을 때 이동할 주소
      * @param Identifier|null $identifier 알림 고유 ID
+     * @param bool $force_silent 강제 무음 설정 여부
      */
-    public function __construct(string $u_id, string $message, string $url, Identifier $identifier = null)
-    {
+    public function __construct(
+        string $u_id,
+        string $message,
+        string $url,
+        Identifier $identifier = null,
+        bool $force_silent = false
+    ) {
         $this->u_id = $u_id;
         $this->message = $message;
         $this->url = $url;
         $this->identifier = $identifier;
+        $this->force_silent = $force_silent;
     }
 
     public function jsonSerialize()
@@ -34,6 +42,7 @@ class ApnsPush implements \JsonSerializable
             'u_id' => $this->u_id,
             'message' => $this->message,
             'url' => $this->url,
+            'force_silent' => $this->force_silent,
         ];
 
         if ($this->identifier !== null) {
