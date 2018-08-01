5c27463aa21950ad71738fb779aa4601be1fd18f
heechan <heechan@ridi.com>
2018-08-01 11:13:45 +0900

    Add default retry middleware with handler

--- src/Ridibooks/Crm/Client.php
+++ src/Ridibooks/Crm/Client.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 namespace Ridibooks\Crm;
 
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Ridibooks\Crm\Notification\Payload\ApnsPush;
 use Ridibooks\Crm\Notification\Payload\Email;
@@ -34,6 +37,40 @@ class Client
         }
 
         $this->client = new GuzzleClient($config);
+    }
+
+    /**
+     * @param array $config
+     * @return Client
+     */
+    public static function createWithDefaultRetry(array $config = [])
+    {
+        if (!array_key_exists('handler', $config)) {
+            $handler = HandlerStack::create();
+            $config['handler'] = $handler;
+        }
+        $config['handler']->push(self::getDefaultRetryMiddleware());
+
+        return new self($config);
+    }
+
+    /**
+     * 5xx 에러가 발생했을때 재시도 합니다.
+     * @param int $max_retry_count 최대 재시도 횟수
+     * @param int $retry_delay 재시도 사이 간격(단위: ms)
+     * @return callable retry 미들웨어
+     */
+    public static function getDefaultRetryMiddleware(int $max_retry_count = 3, int $retry_delay = 1000)
+    {
+        return Middleware::retry(
+            function (int $retries, Request $request, Response $response, $exception) use ($max_retry_count) {
+                $server_error = isset($response) && (floor($response->getStatusCode() / 100) === 5);
+                return $server_error || isset($exception) && $retries < $max_retry_count;
+            },
+            function () use ($retry_delay) {
+                return $retry_delay;
+            }
+        );
     }
 
     /**
