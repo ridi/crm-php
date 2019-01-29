7dd6ceff1bead53044d9e29d029b23bcd25c140c
pueue <dev@pueue.com>
2019-01-29 10:19:20 +0900

    refactor(Client): Remove unused importing

--- src/Ridibooks/Crm/Client.php
+++ src/Ridibooks/Crm/Client.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace Ridibooks\Crm;
 
 use GuzzleHttp\Client as GuzzleClient;
-use GuzzleHttp\Exception\ServerException;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Middleware;
 use GuzzleHttp\Promise\PromiseInterface;
